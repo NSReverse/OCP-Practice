4696d25671d9e6ce4674ae69ff33899b79c1250f
NSReverse <brownrc@live.com>
2018-06-30 01:22:34 -0400

    OCP Objective 2.3 - inner classes

--- src/net/nsreverse/Main.java
+++ src/net/nsreverse/Main.java
@@ -1,8 +1,11 @@
 package net.nsreverse;
 
+import net.nsreverse.objective2_advanced_java_class_design.c_inner_classes.Example1;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Example1 e1 = new Example1();
+        e1.myMethod();
     }
 }
--- src/net/nsreverse/objective2_advanced_java_class_design/c_inner_classes/Example1.java
+++ src/net/nsreverse/objective2_advanced_java_class_design/c_inner_classes/Example1.java
@@ -1,5 +1,7 @@
 package net.nsreverse.objective2_advanced_java_class_design.c_inner_classes;
 
+import net.nsreverse.utils.Log;
+
 /**
  * Example1 -
  *
@@ -9,23 +11,29 @@ public class Example1 {
 
     public class Inner {
         // private static final Integer i = new Integer(0); // Inner classes cannot have static members
+        void doX() {
+            Log.out("Inner class, doX()");
+        }
     }
 
     public void myMethod() {
         Inner inner = new Inner();
+        inner.doX();
     }
 
     public static void main(String[] args) {
         int i = 0;
 
-        /* // Invalid code block. Unable to instantiate this Data1 class because it is not declared properly.
-        Data1 data = new Data1(0);
-
-        class Data1 {
-            int var;
-
-            Data(int var) { this.var = var; }
-        }
-        */
+        /* // Invalid code block. Unable to
+         * // instantiate this Data1 class because it is not declared before instantiating it.
+         *
+         * Data1 data = new Data1(0);
+         *
+         * class Data1 {
+         *   int var;
+         *
+         *   Data(int var) { this.var = var; }
+         * }
+         */
     }
 }
