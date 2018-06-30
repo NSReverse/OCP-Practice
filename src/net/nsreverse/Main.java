32c8825d64fe36eceb8fb6d82b5f409ab9c2971b
NSReverse <brownrc@live.com>
2018-06-30 01:59:14 -0400

    OCP Objective 2.5 - override, extend, implement

--- src/net/nsreverse/Main.java
+++ src/net/nsreverse/Main.java
@@ -1,11 +1,8 @@
 package net.nsreverse;
 
-import net.nsreverse.objective2_advanced_java_class_design.d_enums.Example1;
-
 public class Main {
 
     public static void main(String[] args) {
-        Example1 e1 = new Example1();
-        e1.executeExample();
+
     }
 }
---
+++ src/net/nsreverse/objective2_advanced_java_class_design/e_implement_extend_override_annot/Example1.java
@@ -0,0 +1,27 @@
+package net.nsreverse.objective2_advanced_java_class_design.e_implement_extend_override_annot;
+
+import net.nsreverse.utils.Log;
+
+public class Example1 implements IFace1,
+                                 IFace2 {
+
+    @Override // From IFace1
+    public void methodX() {
+        // Interface-compliant method for IFace1
+        Log.out("Interface-compliant method for IFace1");
+    }
+
+    @Override // From IFace2
+    public void methodY() {
+        // Interface-compliant method for IFace2
+        Log.out("Interface-compliant method for IFace2");
+    }
+}
+
+interface IFace1 {
+    void methodX();
+}
+
+interface IFace2 {
+    void methodY();
+}
